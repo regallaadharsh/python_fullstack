8793be8011737fb07270658501b807a2b5c68402
regallaadharsh <regallaadharsh@gmail.com>
2026-08-05 14:10:21 +0530

    update new folders

--- js/script.js
+++
@@ -1,7 +0,0 @@
-alert("Welcome to NRIIT Learning Management System")
-let heading = document.getElementById("welcome");
-heading.innerHTML = "Welcome Future Software Engineers"
-console.log("Heading element: ", heading)
-
-
-
